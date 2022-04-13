56f06ce77b34f555e8d61ea2413b72b1f2d38477
Weibler23 <103393943+Weibler23@users.noreply.github.com>
2022-04-12 20:42:53 -0600

    Add files via upload

---
+++ organization.java
@@ -0,0 +1,26 @@
+// organization.java
+// Adam Weibler
+// Create a program that contains the menu and new screen objects in a seperate class and use them in the main class
+// Created 1/24/22
+// Modified 1/24/22
+
+public class organization
+{
+    //Frame
+    public static void Frame (int num, boolean newLine)
+    {
+	for (int i = 1; i <= num; i ++)
+	    {
+		System.out.print ('-');
+	    } if (newLine) System.out.print("\n");
+    }
+
+    //ClearScreen
+    public static void ClearScreen ()
+    {
+	for (int i = 1; i <= 100; i++)
+	    {
+		System.out.print('\n');
+	    }
+    }
+}
