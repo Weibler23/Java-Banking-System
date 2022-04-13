86f2825dc8a4c07a22ecfefd01a8def092ad1b50
Weibler23 <103393943+Weibler23@users.noreply.github.com>
2022-04-12 22:17:31 -0600

    Create userInputs.java

---
+++ userInputs.java
@@ -0,0 +1,9 @@
+// userInputs.java
+// Adam Weibler
+// Class to handle input checks for user inputs
+// Created 4/12/22
+
+public class userInputs() {
+    // Create scanner object
+    Scanner input = new Scanner(System.in);
+}
