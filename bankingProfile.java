83e9f6d27cbb284ccc582da96034095d6c232a05
Weibler23 <103393943+Weibler23@users.noreply.github.com>
2022-04-12 20:49:34 -0600

    Create bankingProfile.java

---
+++ bankingProfile.java
@@ -0,0 +1,11 @@
+// bankingProfile.java
+// Adam Weibler
+// Allow the user to create and open profiles
+// Created: 4/12/22
+
+public class bankingProfile {
+    public void openProfile() {
+    }
+    public void createProfile() {
+    }
+}
