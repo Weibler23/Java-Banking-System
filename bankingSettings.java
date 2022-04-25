78c500772983b05cd51d0c21e97d1292e4bdda00
Weibler23 <adam@jdwventures.com>
2022-04-24 19:26:06 -0600

    Fixed slight input bugs

--- bankingSettings.java
+++ bankingSettings.java
@@ -23,6 +23,9 @@ public class bankingSettings {
     // Implement dataStorage class
     dataStorage dS = new dataStorage();
 
+    // Implement organization class
+    organization org = new organization();
+
 public void openSettings(String profileUsername) {
     userProfileUsername = profileUsername + " ProfileSettings.properties";
 
@@ -38,12 +41,15 @@ public void openSettings(String profileUsername) {
         checkDOB = dS.parsedBoolean;
         dS.parseBoolean(prop.getProperty("db.balanceAlerts"));
         balanceAlerts = dS.parsedBoolean;
+        org.ClearScreen();
         HS.settingsHP(profileUsername, checkDOB, balanceAlerts);
         UInp.getMenuInput(5,1);
         switch(UInp.userInput) {   
             case 1:
             System.out.println("* User chose to see settings information *");
             dS.readFullFile("settingsInformation.txt");
+            System.out.print("* Type 'next' to move on *\nInput: ");
+            UInp.getUserInputString("next", true);
             break;
 
             case 2:
@@ -62,6 +68,7 @@ public void openSettings(String profileUsername) {
             UInp.getUserInputChar('y', 'n');
             if (UInp.userInputChar == 'y') {
                 dS.deleteFile(userProfileUsername);
+                settingsRepeat = false;
             } 
             break;
 
